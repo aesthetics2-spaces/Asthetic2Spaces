19878a64dc1ce62ac45a6d3ae70420e18e7d41be
= <=>
2026-01-08 14:30:53 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,7 @@ const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
-  "https://asthetic2spaces1-c7qm.vercel.app",
-  "https://asthetic2spaces-7snq.vercel.app",
-  "https://asthetic2spaces2.vercel.app",
+"https://asthetic2spaces.vercel.app",
   "http://localhost:5173"
 ];
 app.use(cors({
